66c47b4b2adb482a427be393ababf29c10b4cdaf
Eirik L. Vullum <evullum@gmail.com>
2023-02-01 14:16:29 +0100

    Add user feed

--- src/components/UserFeed.js
+++ src/components/UserFeed.js
@@ -1,11 +1,75 @@
 import { Component } from "react";
+import { getTweetsByUsername } from "../services/tweets";
+import Tweet from "./Tweet";
 
 class UserFeed extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tweets: [],
+      isLoading: true,
+      error: null,
+    }
+  }
+
+  async componentDidMount() {
+    await this.handlePopulateTweets();
+  }
+
+  async handlePopulateTweets() {
     const { username } = this.props.match.params;
 
+    this.setState({
+      isLoading: true,
+      error: null,
+    });
+    
+    try {
+      const tweets = await getTweetsByUsername(username);
+    
+      this.setState({
+        tweets,
+        isLoading: false
+      });
+    } catch (error) {
+      this.setState({
+        error: error
+      })
+    }
+  }
+
+  render() {
+    const { username } = this.props.match.params;
+    const { error, isLoading, tweets } = this.state;
+
+    if (error) {
+      return (
+        <div>
+          <h3>Unable to fetch tweets: {error.message}</h3>
+          <button onClick={this.handlePopulateTweets.bind(this)}>
+            Retry
+          </button>
+        </div>
+      );
+    }
+
+    if (isLoading) {
+      return (
+        <div>Loading tweets...</div>
+      );
+    }
+
+    const tweetElements = tweets
+    .map((tweet) => {
+      return <Tweet tweetInfo={tweet} />
+    });
+
     return (
-      <div>User Feed for @{username}</div>
+      <div>
+        <h1>UserFeed for @{username}</h1>
+        <div>{tweetElements}</div>
+      </div>
     );
   }
 }
