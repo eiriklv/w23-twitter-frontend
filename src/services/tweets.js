5ef0a981f06aad9c90f472271bffe97a615bd529
Eirik L. Vullum <evullum@gmail.com>
2023-02-03 12:56:17 +0100

    Use env for api url

--- src/services/tweets.js
+++ src/services/tweets.js
@@ -1,4 +1,4 @@
-const TWITTER_API_URL = 'http://localhost:3333';
+const TWITTER_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3333';
 
 export async function getTweets() {
   const response = await fetch(`${TWITTER_API_URL}/tweets`);
